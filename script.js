ade004dfd9d90a032db97225474a7b269c6f04fc
wesleyogibbons-ctrl <wesley.o.gibbons@gmail.com>
2026-01-28 21:19:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (lastVisit !== today) {
     balance += balance* 0.1;
     balance += 1010
     localStorage.setItem("lastVisitDate", today);
-    alert("Daily Bonus! You got paid 1010$ andYou have gained 10% intrest.");
+    alert("Daily Bonus! You got paid 1010$ and You have gained 10% intrest.");
 }
 
 // Update the screen on load
